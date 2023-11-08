c286cfc50763a398e6410ad463f98d75a6009e6d
LeeZEun <je990420@ajou.ac.kr>
2023-11-08 15:18:03 +0900

    Fix: 입력한 검색어를 결과페이지로 전달하면서 생기는 오류 수정

--- frontend/lib/pages/search/search_page.dart
+++ frontend/lib/pages/search/search_page.dart
@@ -1,4 +1,5 @@
 import 'package:beautyminder/dto/keywordRank_model.dart';
+import 'package:beautyminder/pages/search/search_result_page.dart';
 import 'package:beautyminder/widget/searchAppBar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -13,11 +14,77 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  String searchQuery = ''; // 검색어를 저장할 변수
+  final TextEditingController textController = TextEditingController();
+
+  @override
+  void dispose() {
+    textController.dispose(); // 필요한 경우 컨트롤러를 해제합니다.
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: SearchAppBar(context: context,),
+      appBar: SearchAppBar(title:
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            SizedBox(
+              width: 10,
+            ),
+            Flexible(
+              flex: 1,
+              child: TextField(
+                controller: textController,
+                // onSubmitted: (text) {
+                //   Navigator.of(context).push(MaterialPageRoute(builder: (context) => SearchResultPage(searchQuery: text)),);
+                // },
+                onChanged: (text) {
+                  searchQuery = text;
+                },
+                decoration: const InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 3,
+                    horizontal: 15,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10), //포커스 시
+                    ),
+                    borderSide: BorderSide(
+                      color: Color(0xffd86a04),
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10), // 활성화 상태 모서리를 둥글게 조정
+                    ),
+                    borderSide: BorderSide(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  hintText: "검색 키워드를 입력해주세요.",
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 8,
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) => SearchResultPage(searchQuery: searchQuery)),);
+                print('////////////searchQuery : $searchQuery');
+              },
+              icon: Icon(
+                Icons.search,
+                color: Color(0xffd86a04),
+              ),
+            ),
+          ],
+        ),
+      ),
       body: _searchPageUI(),
     );
   }
--- frontend/lib/widget/searchAppBar.dart
+++ frontend/lib/widget/searchAppBar.dart
@@ -4,71 +4,14 @@ import 'package:flutter/material.dart';
 import '../pages/search/search_result_page.dart';
 
 class SearchAppBar extends AppBar {
-
-  SearchAppBar({Key? key, required BuildContext context})
+  final Widget title;
+  SearchAppBar({Key? key, required this.title})
       :super(
     key: key,
     backgroundColor: Color(0xffffecda),
     elevation: 0,
     centerTitle: false,
-    title: Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        SizedBox(
-          width: 10,
-        ),
-        Flexible(
-          flex: 1,
-          child: TextField(
-            controller: TextEditingController(),
-            onSubmitted: (text) {
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) => SearchResultPage(searchQuery: text)),);
-            },
-            decoration: const InputDecoration(
-              contentPadding: EdgeInsets.symmetric(
-                vertical: 3,
-                horizontal: 15,
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10), //포커스 시
-                ),
-                borderSide: BorderSide(
-                  color: Color(0xffd86a04),
-                ),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10), // 활성화 상태 모서리를 둥글게 조정
-                ),
-                borderSide: BorderSide(
-                  color: Colors.grey,
-                ),
-              ),
-              hintText: "검색 키워드를 입력해주세요.",
-            ),
-          ),
-        ),
-        SizedBox(
-          width: 8,
-        ),
-        IconButton(
-          onPressed: () {
-            String text = TextEditingController().text; // TextField에서 입력한 텍스트 가져오기
-            Navigator.of(context).push(MaterialPageRoute(builder: (context) => SearchResultPage(searchQuery: text,)),);
-          },
-          icon: Icon(
-            Icons.search,
-            color: Color(0xffd86a04),
-          ),
-        ),
-      ],
-    ),
-    // title: Text(
-    //   "BeautyMinder",
-    //   style: TextStyle(color: Color(0xffd86a04)),
-    // ),
+    title: title,
     iconTheme: IconThemeData(
       color: Color(0xffd86a04),
     ),
