769bc6ae88d470462ce12a231a460b43b50f54e9
LeeZEun <je990420@ajou.ac.kr>
2023-11-15 18:51:51 +0900

    Update: Merge with MyPage

--- frontend/lib/pages/my/my_page.dart
+++ frontend/lib/pages/my/my_page.dart
@@ -5,6 +5,7 @@ import 'package:beautyminder/pages/my/user_info_page.dart';
 import 'package:beautyminder/pages/my/widgets/my_divider.dart';
 import 'package:beautyminder/pages/my/widgets/my_page_header.dart';
 import 'package:beautyminder/services/shared_service.dart';
+import 'package:beautyminder/widget/commonAppBar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -42,51 +43,54 @@ class _MyPageState extends State<MyPage> {
   Widget build(BuildContext context) {
     return isLoading
         ? Center(child: Text('로딩 중'))
-        : Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: ListView(
+        : Scaffold(
+      appBar: CommonAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: ListView(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const MyPageHeader('마이페이지'),
-                    const SizedBox(height: 20),
-                    MyPageProfile(
-                        nickname: user!.nickname ?? user!.email,
-                        profileImage: user!.profileImage ?? ''),
-                    const SizedBox(height: 20),
-                    const MyDivider(),
-                    MyPageMenu(
-                        title: '즐겨찾기 해둔 제품',
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const MyFavoritePage()),
-                          );
-                        }),
-                    MyPageMenu(
-                      title: '내가 쓴 리뷰',
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const MyReviewPage()),
-                        );
-                      },
-                    ),
-                    MyPageMenu(
-                      title: '로그아웃',
-                      onTap: () {
-                        SharedService.logout(context);
-                      },
-                    ),
-                    const SizedBox(height: 20),
-                  ],
-                )
+                const MyPageHeader('마이페이지'),
+                const SizedBox(height: 20),
+                MyPageProfile(
+                    nickname: user!.nickname ?? user!.email,
+                    profileImage: user!.profileImage ?? ''),
+                const SizedBox(height: 20),
+                const MyDivider(),
+                MyPageMenu(
+                    title: '즐겨찾기 해둔 제품',
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const MyFavoritePage()),
+                      );
+                    }),
+                MyPageMenu(
+                  title: '내가 쓴 리뷰',
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const MyReviewPage()),
+                    );
+                  },
+                ),
+                MyPageMenu(
+                  title: '로그아웃',
+                  onTap: () {
+                    SharedService.logout(context);
+                  },
+                ),
+                const SizedBox(height: 20),
               ],
-            ),
-          );
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
 
@@ -99,57 +103,59 @@ class MyPageProfile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        SizedBox(
-          width: 50,
-          child: Image.asset(
-            'assets/images/profile.jpg', // profileImage,
-            errorBuilder: (context, error, stackTrace) {
-              return Image.asset('assets/images/profile.jpg');
+    return Material(
+      child: Row(
+        children: [
+          SizedBox(
+            width: 50,
+            child: Image.asset(
+              'assets/images/profile.jpg', // profileImage,
+              errorBuilder: (context, error, stackTrace) {
+                return Image.asset('assets/images/profile.jpg');
+              },
+            ),
+          ),
+          const SizedBox(width: 10),
+          TextButton(
+            child: Text(''),
+            onPressed: () {
+              print(profileImage);
             },
           ),
-        ),
-        const SizedBox(width: 10),
-        TextButton(
-          child: Text(''),
-          onPressed: () {
-            print(profileImage);
-          },
-        ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    nickname,
-                    style: const TextStyle(
-                      fontSize: 15,
-                      color: Color(0xFF585555),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      nickname,
+                      style: const TextStyle(
+                        fontSize: 15,
+                        color: Color(0xFF585555),
+                      ),
+                      textAlign: TextAlign.left,
                     ),
-                    textAlign: TextAlign.left,
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.arrow_right,
-                      color: Color(0xFFFE9738),
+                    IconButton(
+                      icon: Icon(
+                        Icons.arrow_right,
+                        color: Color(0xFFFE9738),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: ((context) => UserInfoPage())));
+                      },
                     ),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: ((context) => UserInfoPage())));
-                    },
-                  ),
-                ],
-              )
-            ],
-          ),
-        )
-      ],
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
@@ -162,29 +168,31 @@ class MyPageMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            title,
-            style: const TextStyle(
-              fontSize: 15,
-              color: Color(0xFF868383),
+    return Material(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 15,
+                color: Color(0xFF868383),
+              ),
+              textAlign: TextAlign.left,
             ),
-            textAlign: TextAlign.left,
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.arrow_right,
-              color: Color(0xFFFE9738),
-            ),
-            onPressed: () {
-              onTap?.call();
-            },
-          )
-        ],
+            IconButton(
+              icon: Icon(
+                Icons.arrow_right,
+                color: Color(0xFFFE9738),
+              ),
+              onPressed: () {
+                onTap?.call();
+              },
+            )
+          ],
+        ),
       ),
     );
   }
