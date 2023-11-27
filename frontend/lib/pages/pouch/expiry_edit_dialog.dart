1e472a5656b08f2ff5eb64d1221483c62c3bf48a
buffl <bufflaw@ajou.ac.kr>
2023-11-27 20:23:43 +0900

    Fix: edit dialog

--- frontend/lib/pages/pouch/expiry_edit_dialog.dart
+++ frontend/lib/pages/pouch/expiry_edit_dialog.dart
@@ -1,7 +1,10 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../../dto/cosmetic_expiry_model.dart';
+import '../../dto/vision_response_dto.dart';
+import '../../services/ocr_service.dart';
 
 class ExpiryEditDialog extends StatefulWidget {
   final CosmeticExpiry expiry;
@@ -48,6 +51,55 @@ class _ExpiryEditDialogState extends State<ExpiryEditDialog> {
     }
   }
 
+  // OCR 페이지로 이동하고 결과를 받아오는 함수
+  Future<void> _navigateAndProcessOCR() async {
+    final PlatformFile? pickedFile = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      withData: true,
+      allowedExtensions: ['png', 'jpg', 'jpeg'],
+    ).then((result) => result?.files.first);
+
+    if (pickedFile != null) {
+      // OCR 처리를 요청하고 결과를 받습니다.
+      try {
+        print("SUCK: ${pickedFile}");
+        final response = await OCRService.selectAndUploadImage(pickedFile);
+        print("FUCK: ${response}");
+        if (response != null) {
+          final VisionResponseDTO result = VisionResponseDTO.fromJson(response);
+          final expiryDateFromOCR = DateFormat('yyyy-MM-dd').parse(result.data);
+
+          // 받아온 유통기한으로 상태 업데이트
+          setState(() {
+            expiryDate = expiryDateFromOCR;
+          });
+        }
+      } catch (e) {
+        print("hello");
+        // 오류 처리
+        _showErrorDialog(e.toString());
+      }
+    } else {
+      _showErrorDialog("No image selected for OCR.");
+    }
+  }
+  // 에러 메시지를 보여주는 함수
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Error'),
+        content: Text(message),
+        actions: [
+          TextButton(
+            child: Text('OK'),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
@@ -71,7 +123,12 @@ class _ExpiryEditDialogState extends State<ExpiryEditDialog> {
             title: Text('유통기한: ${formatDate(expiryDate)}'),
             trailing: Icon(Icons.calendar_today),
             onTap: () => _selectDate(context),
+          ),ListTile(
+            title: Text('OCR'),
+            trailing: Icon(Icons.calendar_today),
+            onTap: () =>_navigateAndProcessOCR() ,
           ),
+
           if (isOpened)
             ListTile(
               title: Text(openedDate != null
